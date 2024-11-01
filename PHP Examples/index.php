b7756395610e691af7bc99075ad2a76d7ba47115
kcraycraft45 <kcraycraft45@southhills.edu>
2024-11-01 12:57:24 -0400

    Test

--- PHP Examples/index.php
+++ PHP Examples/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once './shared/db.inc';
+require_once 'shared/db.inc';
 /**
  * @var mysqli $conn
  */
@@ -16,7 +16,7 @@ if(session_status() != PHP_SESSION_ACTIVE)
 $errors = [];
 $loginSuccess = false;
 
-if($_SERVER['REQUEST_METHOD'] == 'POST')
+if($_SERVER['REQUEST_METHOD'] === 'POST')
 {
     $username = trim($_POST['username']);
     $password = $_POST['password'];
@@ -30,35 +30,28 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
         $errors[] = "Password is required.";
     }
     if(empty($errors)) {
+        $stmt = $conn->prepare("SELECT * FROM LOGINS WHERE (USERNAME = ?) && (PASSWORD = ?) LIMIT 1");
 
 
-        $sql = "SELECT * FROM LOGINS WHERE (USERNAME = '$username') AND (PASSWORD = '$password');";
-        $conn->query($sql);
-        $result = $conn->use_result();
-        foreach($result as $row) {
-
-
-            if ($row) {
-                echo $row['USERNAME'] . "\n";
-                $loginSuccess = true;
-                $message = "Success";
-                echo "<script type='text/javascript'>alert('$message');</script>";
-            }
+        $stmt->bind_param('ss', $username, $password);
+        $stmt->execute();
+        $result = $stmt->store_result();
+        if ($stmt->num_rows === 1) {
+            echo "<script>alert('Logged in Successfully');</script>";
+        } else {
+            echo "<script>alert('Invalid Credentials');</script>";
         }
+        $stmt->close();
+        exit();
     }
-    //echo $usernameAsString;
-    //echo $passwordAsString;
-
-
-    //echo $result;
-
 }
+$conn->close();
 ?>
 
 <html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
-    <title>Parameterized Queries Failed</title>
+    <title>Parameterized Queries Instituted</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
@@ -66,12 +59,22 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
     <main class="log-in">
         <form action="index.php" method="post">
             <h1>Log in</h1>
+            <?php if(empty($errors) === false): ?>
+                <div class="alert alert-danger">
+                    <ul>
+                        <?php foreach($errors as $error): ?>
+                            <li><?= htmlentities($error) ?></li>
+                            <!-- Display errors to the user -->
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
             <div class="form-floating">
                 <input id="username" name="username" type="text" class="form-control" placeholder="name@example.com" required />
                 <label for="username">Email</label>
             </div>
             <div class="form-floating">
-                <input id="password" name="password" type="password" class="form-control" placeholder="Password!123" required />
+                <input id="password" name="password" type="text" class="form-control" placeholder="Password!123" required />
                 <label for="password">Password</label>
             </div>
             <button type="submit" class="btn btn-primary">Log in</button>
